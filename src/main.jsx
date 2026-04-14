4ffdef0ea49d69728737587c448eda3b9393c77a
hobbsmagaret-glitch <hobbs.magaret@gmail.com>
2026-04-14 14:17:02 +1000

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CapeYork2040 from './CapeYork2040';   // adjust name if different
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CapeYork2040 />
+  </React.StrictMode>
+);
